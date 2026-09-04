1e472273341de265bd1941746bc52fd78d62664b
mdrakibmyself <rakibfuadd@gmail.com>
2026-09-04 19:28:11 +0600

    learning f-string

---
+++ 2.4.1.py
@@ -0,0 +1,20 @@
+naam = "Rahim"
+boyosh = 25
+
+#others method
+print("i am " + naam + ", Age" + str(boyosh))
+
+#using f-string
+print(f"i am {naam }, Age{boyosh}")
+
+
+daam= 500
+songkha = 3
+print(f"Total bill: {daam * songkha} Taka")
+
+print(f"Age 5 year later: {boyosh + 5}")
+
+#for seeing Decimal (:.2f)
+
+vag = 10 / 3
+print(f"Fol: {vag:.2f}")
