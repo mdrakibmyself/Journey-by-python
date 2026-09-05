6898c8a56f2dd14f1e292b3658422c4ad240f3b4
mdrakibmyself <rakibfuadd@gmail.com>
2026-09-05 11:01:51 +0600

    Loop

---
+++ 2.6.py
@@ -0,0 +1,10 @@
+#for loop
+for i in range(10):
+    print("Train going to Dhaka..", i)
+
+#while loop
+fuel= 3
+while fuel > 0:
+    print("Train running, Fuel:", fuel)
+    fuel= fuel - 1
+print("Fuel End, Train stop")
