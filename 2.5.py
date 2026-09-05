11e02aedc30b58e88971beef9da904522318896c
mdrakibmyself <rakibfuadd@gmail.com>
2026-09-05 10:53:32 +0600

    Condiotional Operator

---
+++ 2.5.py
@@ -0,0 +1,17 @@
+age = int (input("What old are you"))
+
+if age > 18:
+    print("Youre Adult")
+else:
+    print("You are child")
+    
+number = int(input("Whats your number? "))
+
+if number > 80:
+    print("Grade: A+")
+elif number > 70:
+    print("Grade: A")
+elif number > 60:
+    print("Grade: A-")
+else:
+    print("Try more!")
