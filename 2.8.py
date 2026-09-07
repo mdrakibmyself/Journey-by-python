049e644d79fb25b880e0a08a2fce4fbdc30b8b9a
mdrakibmyself <rakibfuadd@gmail.com>
2026-09-07 21:43:42 +0600

    list in python

---
+++ 2.8.py
@@ -0,0 +1,13 @@
+#list
+jatri = ["rahim" , "karim", "salma"]
+
+print(jatri[0])
+print(jatri[1])
+print(len(jatri))
+
+jatri.append("Kamal")
+print(jatri)
+
+
+for name in jatri:
+    print("Passengers : ", name)
