057e2f0141f186d7a22f83dc918a8fdaa7405e96
mdrakibmyself <rakibfuadd@gmail.com>
2026-09-10 19:04:37 +0600

    OOP in Python

---
+++ oop.py
@@ -0,0 +1,12 @@
+class jatri:
+    def __init__(self,naam,boyosh, seat):
+        self.naam = naam
+        self.boyosh = boyosh
+        self.seat = seat
+jatri1 = jatri("Rakib" , "26" , "A1")
+jatri2 = jatri("Rahim" , "28", "B2")
+
+print(jatri1.naam)
+print(jatri2.naam)
+
+    
